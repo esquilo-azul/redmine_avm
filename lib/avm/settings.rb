679c960cd48aa4c41bbdb0ac7e89902f7bf3e523
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-05-14 22:40:03 -0300

    Situação de tarefa bloqueada. Refs #251.

--- lib/avm/settings.rb
+++ lib/avm/settings.rb
@@ -2,11 +2,11 @@ module Avm
   class Settings
     class << self
       def issue_status_undefined(raise_if_empty = true)
-        if Setting.plugin_avm['issue_status_undefined_id'].present?
-          IssueStatus.find(Setting.plugin_avm['issue_status_undefined_id'])
-        elsif raise_if_empty
-          raise "Situação de tarefa indefida não configurada. Acesse /settings/plugin/avm."
-        end
+        issue_status('issue_status_undefined_id', 'indefida', raise_if_empty)
+      end
+
+      def issue_status_blocked(raise_if_empty = true)
+        issue_status('issue_status_blocked_id', 'bloqueada', raise_if_empty)
       end
 
       def admin_user(raise_if_empty = true)
@@ -14,6 +14,16 @@ module Avm
           User.find(Setting.plugin_avm['admin_user_id'])
         elsif raise_if_empty
           raise "Usuário administrador não configurado. Acesse /settings/plugin/avm."
+        end
+      end
+
+      private
+
+      def issue_status(key, message, raise_if_empty)
+        if Setting.plugin_avm[key].present?
+          IssueStatus.find(Setting.plugin_avm[key])
+        elsif raise_if_empty
+          raise "Situação de tarefa #{message} não configurada. Acesse /settings/plugin/avm."
         end
       end
     end
