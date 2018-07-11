2acb5ad31d5e8e413157a6dab4ee5fd240652736
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-07-11 14:28:00 -0300

    Fix IssueStatusAssignsController issue_field attribute's options.

--- app/controllers/issue_status_assigns_controller.rb
+++ app/controllers/issue_status_assigns_controller.rb
@@ -1,17 +1,21 @@
 class IssueStatusAssignsController < ApplicationController
-  before_action :require_admin
+  before_action :require_admin, :active_scaffold_set_dynamic_options
   layout 'admin_active_scaffold'
-
-  def self.issue_field_options
-    cfs = CustomField.where(type: 'IssueCustomField', field_format: 'user')
-    cf = cfs.first
-    [[cf.id, cf.to_s]]
-    # cfs.map { |n| [n.id, n] }
-  end
 
   active_scaffold :issue_status_assign do |conf|
     conf.columns[:issue_status].form_ui = :select
-    conf.columns[:issue_field].options = { options: issue_field_options }
     conf.columns[:issue_field].form_ui = :select
+  end
+
+  private
+
+  def active_scaffold_set_dynamic_options
+    active_scaffold_config.columns[:issue_field].options =
+      { options: issue_field_options }
+  end
+
+  def issue_field_options
+    CustomField.where(type: 'IssueCustomField', field_format: 'user')
+               .map { |cf| [cf.id, cf.to_s] }
   end
 end
