7f4d0b6f7592ae1ef148fc91d9276d3ba8c3c93d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-05 21:18:38 -0300

    Fix plugin name.

--- lib/avm/patches/test_case_patch.rb
+++ lib/avm/patches/test_case_patch.rb
@@ -3,12 +3,12 @@ module Avm
     module TestCasePatch
       def self.included(base)
         base.setup do
-          s = ::Setting.plugin_avm.dup || {}
+          s = ::Setting.plugin_redmine_avm.dup || {}
           s['issue_status_undefined_id'] = 1
           s['issue_status_blocked_id'] = 4
           s['issue_status_unblocked_id'] = 2
           s['admin_user_id'] = 1
-          ::Setting.plugin_avm = s
+          ::Setting.plugin_redmine_avm = s
           ::ListenerOption.listener_enable('Avm::Listeners::IssueMotivationCheck', false)
         end
       end
