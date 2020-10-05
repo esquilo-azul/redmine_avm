43c036e4fd8058dccd3f29d0443940db35204e22
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-05 18:07:15 -0300

    config/routes.rb: replace "as_routes" by "concerns: :active_scaffold".

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 RedmineApp::Application.routes.draw do
-  resources(:issue_status_assigns) { as_routes }
+  concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
+  resources(:issue_status_assigns, concerns: :active_scaffold)
 end
