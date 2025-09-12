cc94fbf9906c1cec4da54bd76a2292c5d42d23f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 12:52:05 -0300

    Bump to '0.3.14'.

--- lib/redmine_avm/version.rb
+++ lib/redmine_avm/version.rb
@@ -4,7 +4,7 @@ module RedmineAvm
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/redmine_avm'
   SUMMARY = 'AVM support for Redmine.'
-  VERSION = '0.3.13'
+  VERSION = '0.3.14'
 
   module Version
   end
