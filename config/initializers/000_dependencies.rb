bfcbc266548663f07026e17db61394177600a0be
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 13:53:44 -0300

    **/*.rb: adapt to "redmine_events_manager" '0.5.0'.

--- config/initializers/000_dependencies.rb
+++ config/initializers/000_dependencies.rb
@@ -2,5 +2,5 @@
 
 Redmine::Plugin.post_register :redmine_avm do
   # Source: https://github.com/esquilo-azul/redmine_events_manager
-  requires_redmine_plugin(:redmine_events_manager, version_or_higher: '0.16')
+  requires_redmine_plugin(:redmine_events_manager, version_or_higher: '0.5')
 end
--- config/initializers/events_manager.rb
+++ config/initializers/events_manager.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-EventsManager.add_listener(Issue, :create, 'Avm::Listeners::IssueAutoUndefine')
-EventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoUndefine')
-EventsManager.add_listener(IssueRelation, :create, 'Avm::Listeners::IssueAutoUndefine')
-EventsManager.add_listener(IssueRelation, :delete, 'Avm::Listeners::IssueAutoUnblock')
-EventsManager.add_listener(Issue, :delete, 'Avm::Listeners::IssueAutoUnblock')
-EventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoUnblock')
-EventsManager.add_listener(Issue, :create, 'Avm::Listeners::IssueAutoAssign')
-EventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoAssign')
-EventsManager.add_listener(Issue, :update,
-                           'Avm::Listeners::IssueDependenciesSectionCheck')
-EventsManager.add_listener(IssueRelation, :create,
-                           'Avm::Listeners::IssueDependenciesSectionCheck')
+RedmineEventsManager.add_listener(Issue, :create, 'Avm::Listeners::IssueAutoUndefine')
+RedmineEventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoUndefine')
+RedmineEventsManager.add_listener(IssueRelation, :create, 'Avm::Listeners::IssueAutoUndefine')
+RedmineEventsManager.add_listener(IssueRelation, :delete, 'Avm::Listeners::IssueAutoUnblock')
+RedmineEventsManager.add_listener(Issue, :delete, 'Avm::Listeners::IssueAutoUnblock')
+RedmineEventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoUnblock')
+RedmineEventsManager.add_listener(Issue, :create, 'Avm::Listeners::IssueAutoAssign')
+RedmineEventsManager.add_listener(Issue, :update, 'Avm::Listeners::IssueAutoAssign')
+RedmineEventsManager.add_listener(Issue, :update,
+                                  'Avm::Listeners::IssueDependenciesSectionCheck')
+RedmineEventsManager.add_listener(IssueRelation, :create,
+                                  'Avm::Listeners::IssueDependenciesSectionCheck')
 [[Issue, :create], [Issue, :update], [IssueRelation, :create]].each do |v|
-  EventsManager.add_listener(v.first, v.last, 'Avm::Listeners::IssueMotivationCheck')
+  RedmineEventsManager.add_listener(v.first, v.last, 'Avm::Listeners::IssueMotivationCheck')
 end
