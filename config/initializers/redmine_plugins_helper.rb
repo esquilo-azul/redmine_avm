1cc9fada929e1748501b9bee16530472f6ad6eb3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 21:46:19 -0300

    .rubocop.yml#Layout/LineEndStringConcatenationIndentation: remove.

--- config/initializers/redmine_plugins_helper.rb
+++ config/initializers/redmine_plugins_helper.rb
@@ -3,7 +3,7 @@
 default_value = {
   dependencies_section_title: 'Dependencies',
   no_dependencies_section_message: "Dependencies section not found.\n" \
-  "Customize this message in plugin AVM's configuration.",
+                                   "Customize this message in plugin AVM's configuration.",
   dependencies_section_missing_dependencies_message:
     "Missing dependencies found dependencies section: %{ids}.\n" + # rubocop:disable Style/FormatStringToken
     "Customize this message in plugin AVM's configuration.",
